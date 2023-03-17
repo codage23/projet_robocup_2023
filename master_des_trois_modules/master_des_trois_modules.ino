c2755ab3127995dc728b5800ab995575a3be4109
codage23 <ado.codage.23@gmail.com>
2023-03-17 16:31:44 +0100

    corrections de commentaires

--- master_des_trois_modules/master_des_trois_modules.ino
+++ master_des_trois_modules/master_des_trois_modules.ino
@@ -1,14 +1,14 @@
 /***********************************************
        robocup 2023
        arduino uno
-       master des trois modules
+       master des trois modules (portique de detection, convoyeur et bras)
 
 ************************************************/
 
 //=============================
 // inclusion des bibliotheques
 //=============================
-// je charge la bibliothèque qui nous permet de communiquer
+// je charge les bibliothèques qui nous permettent de communiquer
 // avec les composants utilisant le protocole de liaison I2C
 #include <Wire.h>
 #include "fonctions.h"  // fichier des fonctions
@@ -40,10 +40,6 @@ void setup()   {
 
   // Initialise la library Wire et se connecte au bus I2C en tant que master
   Wire.begin();
-
-  values = "mv14";                                 // vitesse du convoyeur a 14
-  SendValue(values, I2C_SLAVE_ADDRESS_CONVOYEUR);  // commande envoyee i2c
-
 }
 
 //=====
@@ -54,60 +50,62 @@ void setup()   {
 // ---------------------------------------------------------------------------
 void loop() {
 
-  values = "pc";  // demande couleur
-  SendValue(values, I2C_SLAVE_ADDRESS_PORTIQUE);    // commande envoiyee i2c
+  values = "mv14";                                 // vitesse du convoyeur a 14
+  SendValue(values, I2C_SLAVE_ADDRESS_CONVOYEUR);  // commande envoyee i2c
+
+  values = "pc";                                    // demande couleur aupres du portique
+  SendValue(values, I2C_SLAVE_ADDRESS_PORTIQUE);    // commande envoyee i2c
   Wire.requestFrom(I2C_SLAVE_ADDRESS_PORTIQUE, 1);  // lecture de la valeur en retour
   if (Wire.available())  {                          // attente des octets i2c
     char c = Wire.read();
     if ( c == 1 ) {     // couleur rouge detectee
-      values = "pl10";  // led rouge
-      SendValue(values, I2C_SLAVE_ADDRESS_PORTIQUE); // commande envoiyee i2c
+      values = "pl10";  // allumage led rouge
+      SendValue(values, I2C_SLAVE_ADDRESS_PORTIQUE); // commande envoyee i2c
       couleur = 10;
     } else if (c == 2) {  // couleur verte detectee
-      values = "pl20";    // led verte
-      SendValue(values, I2C_SLAVE_ADDRESS_PORTIQUE); // commande envoiyee i2c
+      values = "pl20";    // allumage led verte
+      SendValue(values, I2C_SLAVE_ADDRESS_PORTIQUE); // commande envoyee i2c
       couleur = 20;
     } else if (c == 3) {  // couleur bleue detectee
-      values = "pl30";    // led bleue
-      SendValue(values, I2C_SLAVE_ADDRESS_PORTIQUE); // commande envoiyee i2c
+      values = "pl30";    // allumage led bleue
+      SendValue(values, I2C_SLAVE_ADDRESS_PORTIQUE); // commande envoyee i2c
       couleur = 30;
     } else {              // aucune couleur de detectee
       values = "pl00";    // led eteinte
-      SendValue(values, I2C_SLAVE_ADDRESS_PORTIQUE); // commande envoiyee i2c
+      SendValue(values, I2C_SLAVE_ADDRESS_PORTIQUE); // commande envoyee i2c
       couleur = 0;
     }
   }
-
   delay(300);
 
-  values = "sc";  // demande etat objet sur le convoyeur
+  values = "sc";                                     // demande de l'etat ded l'objet sur le convoyeur
   SendValue(values, I2C_SLAVE_ADDRESS_CONVOYEUR);    // envoi i2cI2C_SLAVE_ADDRESS_PORTIQUE
   Wire.requestFrom(I2C_SLAVE_ADDRESS_CONVOYEUR, 1);  // lecture de la valeur en retour
   if (Wire.available())  {                           // attente des octets i2c
     char c = Wire.read();
     if ( c == 1 ) {                                  // pas d'objet en attente sur le convoyeur
       Serial.println("objet absent ");
-      values = "START"; I2C_SLAVE_ADDRESS_PORTIQUE;   // commande START
-      SendValue(values, I2C_SLAVE_ADDRESS_CONVOYEUR); // commande envoiyee i2c
+      values = "START";                               // commande START du convoyeur
+      SendValue(values, I2C_SLAVE_ADDRESS_CONVOYEUR); // commande envoyee i2c
     } else {
       Serial.println("objet present ");
-      values = "STOP";
-      SendValue(values, I2C_SLAVE_ADDRESS_CONVOYEUR); // commande envoiyee i2c
+      values = "STOP";                                // commande STOP du convoyeur
+      SendValue(values, I2C_SLAVE_ADDRESS_CONVOYEUR); // commande envoyee i2c
       if (couleur == 10) {    // couleur rouge
         positionAttente();    // fonction attente
         delay(1000);
         attraper();           // fonction attraper
         delay(1000);
         values = "s1130";     // taille position 130
-        SendValue(values, I2C_SLAVE_ADDRESS_BRAS);    // commande envoiyee i2c
+        SendValue(values, I2C_SLAVE_ADDRESS_BRAS);    // commande envoyee i2c
         delay(1000);
         values = "s3130";     // coude position 130
-        SendValue(values, I2C_SLAVE_ADDRESS_BRAS);    // commande envoiyee i2c
+        SendValue(values, I2C_SLAVE_ADDRESS_BRAS);    // commande envoyee i2c
         delay(1000);
         relacher();           // fonction relacher
         delay(1000);
         values = "s380";      // coude position 80
-        SendValue(values, I2C_SLAVE_ADDRESS_BRAS);    // commande envoiyee i2c
+        SendValue(values, I2C_SLAVE_ADDRESS_BRAS);    // commande envoyee i2c
         delay(1000);
         positionAttente();     // fonction attente
         delay(1000);
@@ -117,15 +115,15 @@ void loop() {
         attraper();                // fonction attraper
         delay(1000);
         values = "s175";           // taille position 75
-        SendValue(values, I2C_SLAVE_ADDRESS_BRAS);    // commande envoiyee i2c
+        SendValue(values, I2C_SLAVE_ADDRESS_BRAS);    // commande envoyee i2c
         delay(1000);
         values = "s3130";           // coude position 130
-        SendValue(values, I2C_SLAVE_ADDRESS_BRAS);    // commande envoiyee i2c
+        SendValue(values, I2C_SLAVE_ADDRESS_BRAS);    // commande envoyee i2c
         delay(1000);
         relacher();                 // fonction relacher
         delay(1000);
         values = "s380";            // coude position 80
-        SendValue(values, I2C_SLAVE_ADDRESS_BRAS);    // commande envoiyee i2c
+        SendValue(values, I2C_SLAVE_ADDRESS_BRAS);    // commande envoyee i2c
         delay(1000);
         positionAttente();          // fonction attente
         delay(1000);
@@ -135,18 +133,18 @@ void loop() {
         attraper();               // fonction attraper
         delay(1000);
         values = "s1100";         // taille position 100
-        SendValue(values, I2C_SLAVE_ADDRESS_BRAS);    // commande envoiyee i2c
+        SendValue(values, I2C_SLAVE_ADDRESS_BRAS);    // commande envoyee i2c
         delay(1000);
         values = "s2170";         // epaule position 170
-        SendValue(values, I2C_SLAVE_ADDRESS_BRAS);    // commande envoiyee i2c
+        SendValue(values, I2C_SLAVE_ADDRESS_BRAS);    // commande envoyee i2c
         delay(1000);
         values = "s3140";         // coude position 140
-        SendValue(values, I2C_SLAVE_ADDRESS_BRAS);    // commande envoiyee i2c
+        SendValue(values, I2C_SLAVE_ADDRESS_BRAS);    // commande envoyee i2c
         delay(1000);
         relacher();               // fonction relacher
         delay(1000);
         values = "s380";          // coude position 80
-        SendValue(values, I2C_SLAVE_ADDRESS_BRAS);    // commande envoiyee i2c
+        SendValue(values, I2C_SLAVE_ADDRESS_BRAS);    // commande envoyee i2c
         delay(1000);
         positionAttente();        // fonction attente
         delay(1000);
@@ -156,18 +154,18 @@ void loop() {
         attraper();         // fonction attraper
         delay(1000);
         values = "s1160";    // taille position 160
-        SendValue(values, I2C_SLAVE_ADDRESS_BRAS);    // commande envoiyee i2c
+        SendValue(values, I2C_SLAVE_ADDRESS_BRAS);    // commande envoyee i2c
         delay(1000);
         values = "s2170";   // epaule position 170
-        SendValue(values, I2C_SLAVE_ADDRESS_BRAS);    // commande envoiyee i2c
+        SendValue(values, I2C_SLAVE_ADDRESS_BRAS);    // commande envoyee i2c
         delay(1000);
         values = "s3140";   // coude position 140
-        SendValue(values, I2C_SLAVE_ADDRESS_BRAS);    // commande envoiyee i2c
+        SendValue(values, I2C_SLAVE_ADDRESS_BRAS);    // commande envoyee i2c
         delay(1000);
         relacher();         // fonction relacher
         delay(1000);
         values = "s380";    // coude position 80
-        SendValue(values, I2C_SLAVE_ADDRESS_BRAS);    // commande envoiyee i2c
+        SendValue(values, I2C_SLAVE_ADDRESS_BRAS);    // commande envoyee i2c
         delay(1000);
         positionAttente();  // fonction attente
         delay(1000);
